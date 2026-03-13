53b148ed7f53ca7adb8ee0af32f040484aced08d
Ekaitz Alonso Aurrekoetxea <1012563D@H015110.NET>
2026-03-13 11:54:45 +0100

    Web adaptatuta lan berriari

---
+++ Web orria/index.php
@@ -0,0 +1,59 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="irudiak/Ikono-removebg-preview.png" type="image/x-icon">
+    <link rel="stylesheet" href="estiloa/nireestiloa.css">
+    <script src="js/jquery-3.7.1.min.js"></script>
+    <script src="js/nirescript.js"></script>
+    <title>FNFS - Hasiera</title>
+</head>
+
+<body>
+    <header>
+        <!-- LOGOA WEB ORRIALDEAREN GOIKO PARTEAN HANDIAN -->
+        <a id="hasieraLogo"><img src="irudiak/FNFS Logo blanco transparente.png" alt="FNFS Logo" class="logoa"></a>
+        <!-- WEB ORRIALDEAREN NABEGAZIO MENUA -->
+        <nav>
+            <div class="menuBotoia"><a href="">Iradokizunak</a></div>
+            <div class="menuBotoia"><a href="">Taldeak</a></div>
+            <div class="menuBotoia"><a href="">Hasiera</a></div>
+            <div class="menuBotoia"><a href="">Sailkapena</a></div>
+            <div class="menuBotoia"><a href="">Fitxaketak</a></div>
+            <div class="menuBotoia"><a href="">Partiduak</a></div>
+        </nav>
+    </header>
+    <main>
+        <article>
+            <h1 class="hasierakoTitulua">Nortzuk Gara?</h1>
+            <div class="hasieraArtikulo">
+                <img src="irudiak/PortadaIrudi.jpeg" alt="logo">
+                <p>
+                    Ongi etorri FNFS-ra. Gu areto futbola antolatu, sustatu eta garatzeaz arduratzen den federazioa gara. Gure helburu nagusia kirol honenganako pasioa bazter guztietara zabaltzea da, oinarrizko mailetatik hasi eta kategoria gorenetaraino. Uste irmoa dugu kirola gizartea eraldatzeko eta pertsonak batzeko tresna indartsua dela, eta horregatik, kalitatezko lehiaketak eskaintzeko lan egiten dugu egunero, beti ere gure kideen gozamena lehenetsiz.
+                    <br><br>
+                    Gure jardunaren ardatza kantxan ematen den jokoa baino askoz haratago doa. FNFS-n talde-lana, kiroltasuna, errespetua eta esfortzua bezalako oinarrizko balioak bultzatzen ditugu. Jokalarien garapen integrala bilatzen dugu, arlo fisikoan zein pertsonalean hazteko aukera eskainiz, eta guztiontzako gune seguru, inklusibo eta parekide bat bermatuz. Areto futbola ez da soilik baloi bat ate batean sartzea; laguntasuna, disziplina eta ilusioa partekatzea da.
+                    <br><br>
+                    Beraz, jokalaria, entrenatzailea, epailea zein zalea izan, hemen zure etxea aurkituko duzu. Gure txapelketetan parte hartzera, gure proiektuetan murgiltzera eta astebururo kantxako emozio oro gurekin partekatzera gonbidatzen zaitugu. Egin bat gure komunitatearekin, jantzi zapatilak eta bizi areto futbola gurekin!
+                </p>
+            </div>
+        </article>
+    </main>
+    <footer>
+        <div class="kontaktua">
+            <a href="https://www.google.com/maps/place/P.%C2%BA+de+la+Castellana,+151,+4%C2%BAB,+Tetu%C3%A1n,+28046+Madrid/@40.4608003,-3.6930766,663m/data=!3m2!1e3!4b1!4m6!3m5!1s0xd42291b0928b133:0x9b65449e87642aa0!8m2!3d40.4608003!4d-3.6905017!16s%2Fg%2F11lkzhrg_k?entry=ttu&g_ep=EgoyMDI1MTAwNi4wIKXMDSoASAFQAw%3D%3D"
+                target="_blank" class="">P.º de la Castellana, 151, 4ºB</a>
+            <p>+34 91 350 25 01</p>
+            <a href="mailto:fnfs@fnfs.es" class="">fnfs@fnfs.es</a>
+        </div>
+        <div class="links">
+            <a href="https://www.instagram.com/lnfs89" class="icono instagram" target="_blank"></a>
+            <a href="https://www.youtube.com/channel/UCiSSamUOaeCFQS9MXVqhOPw" class="icono youtube" target="_blank"></a>
+            <a href="https://www.tiktok.com/@lnfs89?lang=es" class="icono tiktok" target="_blank"></a>
+        </div>
+    </footer>
+</body>
+
+</html>
