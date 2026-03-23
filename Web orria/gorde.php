b5b44bd846ed0d6e974c050c54860d33cf0b5cf9
Ekaitz Alonso Aurrekoetxea <1012563D@H015110.NET>
2026-03-23 10:11:27 +0100

    kontaktu aukera badoa eta xml bides gordetsen du informazioa, ez da web orrian inon bistaratsen.

---
+++ Web orria/gorde.php
@@ -0,0 +1,52 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    $gaia = $_POST['gaia'];
+    $mezua = $_POST['mezua'];
+    $izena = $_POST['izena'];
+    $abizena = $_POST['abizena'];
+    $email = $_POST['email'];
+    $telefonoa = $_POST['telefonoa'];
+    
+    // Eguna eta ordua gordetzen ditugu
+    $data_ordua = date('Y-m-d H:i:s');
+
+    $fitxategia = 'iradokizunak.xml';
+
+    // Fitxategia existitzen den begiratzen dugu
+    if (file_exists($fitxategia)) {
+        $xml = simplexml_load_file($fitxategia);
+    } else {
+        $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><iradokizunak></iradokizunak>');
+    }
+
+    $iradokizun_berria = $xml->addChild('iradokizuna');
+    $iradokizun_berria->addChild('data', $data_ordua);
+    $iradokizun_berria->addChild('gaia', $gaia);
+    $iradokizun_berria->addChild('mezua', $mezua);
+    $iradokizun_berria->addChild('izena', $izena);
+    $iradokizun_berria->addChild('abizena', $abizena);
+    $iradokizun_berria->addChild('email', $email);
+    $iradokizun_berria->addChild('telefonoa', $telefonoa);
+
+    
+    // XML dokumentu berri bat prestatzen dugu formatua emateko
+    $dom = new DOMDocument('1.0');
+    // Aurretik zeuden zuriuneak ahazteko esaten diogu
+    $dom->preserveWhiteSpace = false;
+    // Saltoak eta tabulazioak aktibatzen ditugu
+    $dom->formatOutput = true;
+
+    $dom->loadXML($xml->asXML());
+    
+    $dom->save($fitxategia);
+
+    header("Location: kontaktu.php?egoera=ondo");
+    exit();
+
+} else {
+    // Norbait formularioa bete gabe sartzen bada
+    header("Location: index.php");
+    exit();
+}
+?>
--- Web orria/kontaktu.php
+++ Web orria/kontaktu.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="irudiak/Ikono-removebg-preview.png" type="image/x-icon">
     <link rel="stylesheet" href="estiloa/nireestiloa.css">
-    <title>FNFS - Hasiera</title>
+    <title>FNFS - Iradokizunak</title>
 </head>
 
 <body>
@@ -32,8 +32,16 @@
             <?php endif; ?>
         </div>
     </header>
+
     <main class="iradokizun-main">
         <div class="formulario-txartela">
+            
+            <?php if(isset($_GET['egoera']) && $_GET['egoera'] == 'ondo'): ?>
+                <div class="mezu-berdea">
+                    Iradokizuna ondo gorde da! Eskerrik asko.
+                </div>
+            <?php endif; ?>
+            
             <form action="gorde.php" method="POST" class="iradokizun-form">
                 
                 <div class="input-gaia-taldea">
@@ -47,26 +55,22 @@
                 <div class="input-grid-2x2">
                     <div class="input-taldea-iradokizun">
                         <label for="izena" class="etiketa-beltza">Izena</label>
-                        <input type="text" name="izena" class="iradokizun-input" required>
+                        <input type="text" name="izena" id="izena" class="iradokizun-input" required>
                     </div>
                     
                     <div class="input-taldea-iradokizun">
                         <label for="abizena" class="etiketa-beltza">Abizena</label>
-                        <input type="text" name="abizena" class="iradokizun-input" required>
+                        <input type="text" name="abizena" id="abizena" class="iradokizun-input" required>
                     </div>
                     
                     <div class="input-taldea-iradokizun">
                         <label for="email" class="etiketa-beltza">Posta</label>
-                        <div class="input-ikonoarekin">
-                            <input type="email" name="email" required>
-                        </div>
+                        <input type="email" name="email" id="email" class="iradokizun-input" required>
                     </div>
                     
                     <div class="input-taldea-iradokizun">
                         <label for="telefonoa" class="etiketa-beltza">Telefonoa</label>
-                        <div class="input-ikonoarekin">
-                            <input type="text" name="telefonoa" pattern="[0-9]{9}" required>
-                        </div>
+                        <input type="text" name="telefonoa" id="telefonoa" class="iradokizun-input" pattern="[0-9]{9}" required>
                     </div>
                 </div>
 
@@ -77,12 +81,12 @@
             </form>
         </div>
     </main>
+
     <footer>
         <div class="kontaktua">
-            <a href="https://www.google.com/maps/place/P.%C2%BA+de+la+Castellana,+151,+4%C2%BAB,+Tetu%C3%A1n,+28046+Madrid/@40.4608003,-3.6930766,663m/data=!3m2!1e3!4b1!4m6!3m5!1s0xd42291b0928b133:0x9b65449e87642aa0!8m2!3d40.4608003!4d-3.6905017!16s%2Fg%2F11lkzhrg_k?entry=ttu&g_ep=EgoyMDI1MTAwNi4wIKXMDSoASAFQAw%3D%3D"
-                target="_blank" class="">P.º de la Castellana, 151, 4ºB</a>
+            <a href="https://www.google.com/maps/place/P.%C2%BA+de+la+Castellana,+151,+4%C2%BAB,+Tetu%C3%A1n,+28046+Madrid/@40.4608003,-3.6930766,663m/data=!3m2!1e3!4b1!4m6!3m5!1s0xd42291b0928b133:0x9b65449e87642aa0!8m2!3d40.4608003!4d-3.6905017!16s%2Fg%2F11lkzhrg_k?entry=ttu&g_ep=EgoyMDI1MTAwNi4wIKXMDSoASAFQAw%3D%3D" target="_blank">P.º de la Castellana, 151, 4ºB</a>
             <p>+34 91 350 25 01</p>
-            <a href="mailto:fnfs@fnfs.es" class="">fnfs@fnfs.es</a>
+            <a href="mailto:fnfs@fnfs.es">fnfs@fnfs.es</a>
         </div>
         <div class="links">
             <a href="https://www.instagram.com/lnfs89" class="icono instagram" target="_blank"></a>
